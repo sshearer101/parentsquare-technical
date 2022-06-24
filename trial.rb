57454a3c763f6ab3dfcf2fad30d22159efb9ea37
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-06-24 14:39:49 -0600

    1, 2, 4

--- trial.rb
+++ trial.rb
@@ -1,13 +1,17 @@
 require 'csv'
+require 'set'
+
+
 
 # 1: Print the 3 most commonly used languages and the count of parents for each language.
 
-parents = CSV.parse(File.read("parents.csv"), headers: true)
+# parents = CSV.parse(File.read("parents.csv"), headers: true)
 
-language = parents["language"].group_by{|x| x}.transform_values{|values| values.count}.sort_by{|k, v| -v}
+# p parents
+# language = parents["language"].group_by{|x| x}.transform_values{|values| values.count}.sort_by{|k, v| -v}
 
 
-puts language.first(3)
+# puts language.first(3)
 
 
 # avoid doing everything on one line (for each method, do separate variable)
@@ -18,12 +22,12 @@ puts language.first(3)
 
 
 
-students = CSV.parse(File.read("students.csv"), headers: true)
+# students = CSV.parse(File.read("students.csv"), headers: true)
 
-no_cellphone = students.select{|student| student["cellphone"] == nil}.map do |n| "First Name: #{n['FirstName']} Last Name: #{n['LastName']} Student ID: #{n['student_id']}" end 
+# no_cellphone = students.select{|student| student["cellphone"] == nil}.map do |n| "First Name: #{n['FirstName']} Last Name: #{n['LastName']} Student ID: #{n['student_id']}" end 
 
 
-puts no_cellphone
+# puts no_cellphone
 
 
 
@@ -73,29 +77,27 @@ puts no_cellphone
 #     data1["student_id"].merge(data2["student_id"])
 # end
 
-file1_lines = CSV.read("parents.csv")
-file2_lines = CSV.read("students.csv")
-
-studentArray = []
-
-newArray=[]
-
-# for i in 0..studentArray.size
-#     newArray.push(studentArray[i])
-# end
+parents_csv = CSV.read("parents.csv")
+students_csv = CSV.read("students.csv")
 
 
+parent_si=[]
+student_si=[]
 
-trialArray=[]
-
-for i in 1..file1_lines.size
-    # studentArray.push(file1_lines[i][1])
-#   puts file1_lines[i][1]
-    # puts file1_lines[i][1]
-    trialArray.push(file1_lines[i][1])
+parents_csv.filter do |x|
+    parent_si.push(x[1])
 end
 
-puts trialArray
+students_csv.filter do |x|
+    student_si.push(x[1])
+end
+
+for i in 0..student_si.size
+    for i in 0..parent_si.size
+        if
+    end
+end
+
 # for i in 0..file1_lines.size
 #   if (file1_lines[0][1] == file2_lines[0][1])
 #     puts "Same #{file1_lines[i]}"
@@ -103,4 +105,3 @@ puts trialArray
 #     puts "#{file1_lines[i]} != #{file2_lines[i]}"
 #   end
 # end
-
