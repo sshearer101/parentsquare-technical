2e1dd270794aa3cde35cdbed1fea58983b36d98f
Sam Shearer <85294886+sshearer101@users.noreply.github.com>
2022-06-25 17:31:06 -0600

    fifth

--- 5.rb
+++ 5.rb
@@ -4,7 +4,22 @@ require 'csv'
 # 5. Print a list of sections (section_id, course_name) who do not have any students enrolled.
 
 sections_csv = CSV.parse(File.read("sections.csv"), headers: true)
-students_csv = CSV.parse(File.read("students.csv"), headers: true)
+rosters_csv = CSV.parse(File.read("rosters.csv"), headers: true)
+
+# capture the section ids of the rosters and sections
+rosters_section_ids = rosters_csv["section_id"]
+sections_section_ids = sections_csv["section_id"]
+
+# compare the arrays and eliminate instances of sections that have students rostered
+no_students_enrolled_sections =  sections_section_ids - rosters_section_ids
+
+# filter through all the sections and match them with the sections that have no students, and return the values of those sections
+sections_csv.filter do |x|
+    no_students_enrolled_sections.filter do |i|
+        if(x["section_id"] == i)
+            puts "#{x["school_id"]} #{x["section_id"]} #{x["staff_id"]} #{x["course_name"]}"
+        end
+    end
+end
 
 
-puts sections_csv
